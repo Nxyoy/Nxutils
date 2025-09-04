0e6602afc4db687ea9f9fd4a4cac2fcd8f771a80
Nxyoy <58658254+Nxyoy@users.noreply.github.com>
2025-09-04 12:06:57 +0300

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,1 +1,15 @@
+function calcInput(value) {
+  document.getElementById("calc-display").value += value;
+}
+function calculate() {
+  try {
+    let result = eval(document.getElementById("calc-display").value);
+    document.getElementById("calc-display").value = result;
+  } catch {
+    document.getElementById("calc-display").value = "Error";
+  }
+}
+function clearCalc() {
+  document.getElementById("calc-display").value = "";
+}
 
